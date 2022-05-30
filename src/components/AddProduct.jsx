d7ab0b2b0e6199355123589a8f14152264993931
DEEPAK KUMAR MANDAL <nomercyhunters@gmail.com>
2022-05-30 16:17:30 +0530

    done

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,34 +1,141 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Input,
+  Select,
+  RadioGroup,
+  Radio,
+  useDisclosure,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 const AddProduct = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const Modal = () => <div />;
-  const ModalBody = () => <div />;
-  const Input = () => <div />;
-  const Select = () => <div />;
-  const RadioGroup = () => <div />;
-  const Radio = () => <div />;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [querry, setquerry] = useState({
+    title: "",
+    category: "",
+    gender: "",
+    imageSrc: "",
+    price: "",
+  });
+
+ 
+  function storedata(e) {
+    if (typeof e != "string") {
+      const { name, value, checked, type } = e.target;
+      setquerry({ ...querry, [name]: value });
+    } else {
+      setquerry({ ...querry, gender: e });
+    }
+    
+  }
+ 
+ useEffect(() => {
+   fetch("http://localhost:8080/products")
+  .then((r) => r.json())
+  .then((d) => setquerry(d));
+
+console.log(querry);
+  
+ }, [])
+ 
+
+
+  function senddata(e) {
+    e.preventDefault();
+    fetch("http://localhost:8080/products",{
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify( {...querry} ),
+    
+    })
+      .then((r) => r.json())
+      .then((d) => setquerry([...querry,d]));
+  }
 
   return (
     <>
-      <Button my={4} data-cy="add-product-button"></Button>
-      <Modal>
-        <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
-          <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
-          </Select>
-          <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
-          </RadioGroup>
-          <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
-        </ModalBody>
+      <Button onClick={onOpen} my={4} data-cy="add-product-button">
+        Add New Product{" "}
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={5}>
+            <p>Title</p>
+            <Input
+              data-cy="add-product-title"
+              name="title"
+              onChange={storedata}
+              placeholder="Title"
+            />
+            <p>Category</p>
+            <Select
+              data-cy="add-product-category"
+              name="category"
+              onChange={storedata}
+            >
+              <option data-cy="add-product-category-Category">Category</option>
+              <option data-cy="add-product-category-shirt">shirt</option>
+              <option data-cy="add-product-category-pant">pant</option>
+              <option data-cy="add-product-category-jeans">jeans</option>
+            </Select>
+            <p>Gender</p>
+            <RadioGroup
+              data-cy="add-product-gender"
+              name="gender"
+              onChange={storedata}
+            >
+              <Radio
+                data-cy="add-product-gender-male"
+                name="gender2"
+                value={"male"}
+              >
+                male
+              </Radio>
+              <Radio
+                data-cy="add-product-gender-female"
+                name="gender2"
+                value={"female"}
+              >
+                female
+              </Radio>
+              <Radio
+                data-cy="add-product-gender-unisex"
+                name="gender2"
+                value={"unisex"}
+              >
+                unisex
+              </Radio>
+            </RadioGroup>
+            <p>Price</p>
+            <Input
+              data-cy="add-product-price"
+              name="price"
+              onChange={storedata}
+              placeholder="Price"
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button data-cy="add-product-submit-button" onClick={senddata}>
+              Create
+            </Button>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
     </>
   );
